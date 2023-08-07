fbce810266a682cae00871b4c793537848ae5909
moth <74442751+Gincaza@users.noreply.github.com>
2023-08-07 10:56:46 +0100

    .

--- dcrm/website/views.py
+++ dcrm/website/views.py
@@ -1,3 +1,24 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
 
-# Create your views here.
+def home(request):
+    # Check if the user is auth
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        # Authenticate
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            messages.success(request, "You are logged in")
+            return redirect('home')
+        else:
+            messages.error(request, "There was an error.")
+            return redirect('home')  # Redirecionar de volta para a página de login
+    else:
+        return render(request, 'home.html', {})
+
+
+def logout_user(request):
+    pass
